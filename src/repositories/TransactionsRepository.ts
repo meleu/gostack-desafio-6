fd3779db07224b70fced6f0b27125924a8f7e713
meleu <meleuzord@yahoo.com>
2020-11-11 18:01:24 -0300

    just starting

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,30 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = this.find();
+    const balance = (await transactions).reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            accumulator.total += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            accumulator.total -= transaction.value;
+            break;
+          default:
+        }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,50 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    if (!title || !value || !type || !category) {
+      throw new AppError('Invalid input, check if all fields are filled.');
+    }
+
+    const categoryRepository = getRepository(Category);
+
+    let existentCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+    if (!existentCategory) {
+      const newCategory = categoryRepository.create({ title: category });
+      existentCategory = await categoryRepository.save(newCategory);
+    }
+
+    const transactionsRepository = getRepository(Transaction);
+    const newTransaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: existentCategory.id,
+    });
+
+    await transactionsRepository.save(newTransaction);
+
+    return newTransaction;
   }
 }
 
